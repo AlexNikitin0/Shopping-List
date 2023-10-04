40fc418afcf7369327b9fceeed7ba7c8ee8faf62
anikiti3 <anikiti3@email.cpcc.edu>
2023-10-04 10:48:13 -0400

    First commit!

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Shopping list: \n 1.carrots \n 2.potatos \n 3.bread \n 4.salsa \n 5.steak \n 6.takis \n 7.sprite \n 8.cheddar \n 9.hot sauce \n 10.bagels");
+    }
+
+}
